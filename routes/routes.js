4891b55a5a3a99004bcd80fe6af814d50ec8bbfe
farhan <farhanfahim79@gmail.com>
2024-02-10 23:45:05 +0600

    removed all unnecessery files for base repo

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express-promise-router")();
-
-const RouteController = require("../apiController/routes").RouteController;
-let routeController = new RouteController();
-
-// add new endpoints here
-router.get("/", routeController.getOptimalRoute);
-
-module.exports = router;
--- routes/tickets.js
+++
@@ -1,9 +0,0 @@
-const router = require("express-promise-router")();
-
-const TicketController = require("../apiController/tickets").TicketController;
-let ticketController = new TicketController();
-
-// add new endpoints here
-router.post("/", ticketController.purchaseTicket);
-
-module.exports = router;
